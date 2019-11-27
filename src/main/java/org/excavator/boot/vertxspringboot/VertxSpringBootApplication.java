39bffe33da075d26155897f1ee3dc924d5fe0269
CMonkey <42.codemonkey@gmail.com>
2019-11-27 14:21:41 +0800

    init project

---
+++ src/main/java/org/excavator/boot/vertxspringboot/VertxSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.excavator.boot.vertxspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertxSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VertxSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/excavator/boot/vertxspringboot/VertxSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.excavator.boot.vertxspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VertxSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
