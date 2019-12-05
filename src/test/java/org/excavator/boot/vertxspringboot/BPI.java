24b606780cc59e14e63e182719ac8561f5f55e01
CMonkey <42.codemonkey@gmail.com>
2019-12-05 11:22:04 +0800

    add BPI

---
+++ src/test/java/org/excavator/boot/vertxspringboot/BPI.java
@@ -0,0 +1,34 @@
+package org.excavator.boot.vertxspringboot;
+
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.synchronoss.cloud.nio.multipart.util.IOUtils;
+
+import javax.json.Json;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
+public class BPI{
+    public static void main (String [] args) throws IOException, InterruptedException {
+        var request = HttpRequest
+            .newBuilder()
+            .uri(URI.create("https://api.coindesk.com/v1/bpi/currentprice.json"))
+            .GET()
+            .build();
+
+        var response = HttpClient
+            .newHttpClient()
+            .send(request, HttpResponse.BodyHandlers.ofInputStream());
+
+        var responseBody = response.body();
+        String r = IOUtils.inputStreamAsString(responseBody, StandardCharsets.UTF_8.name());
+        var json = JSON.parseObject(r);
+        var bpi = json.getJSONObject("BPI");
+        System.out.println("bpi = " + bpi);
+        //System.out.printf("Current Bitcoin Price: $%s %s", price, indicator);
+    }
+}
